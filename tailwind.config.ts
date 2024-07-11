4faa6749e6c171875621851158ad58680e3e719a
molaraiche <laraichemohamed@gmail.com>
2024-07-11 01:18:29 +0100

    updates: set tailwind fonts,colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,34 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: {
+          darkBlue: '#181E4B',
+          titleColor: '#14183E',
+          textColor: '#5E6282',
+          orange: '#DF6951',
+          yellow: '#F1A501',
+        },
+      },
+      fontFamily: {
+        Poppins: ['var(--font-Poppins)'],
+        Volkhov: ['var(--font-Volkhov)'],
+      },
+      fontSize: {},
+      boxShadow: {
+        'light-100':
+          '0px 12px 20px 0px rgba(184, 184, 184, 0.03), 0px 6px 12px 0px rgba(184, 184, 184, 0.02), 0px 2px 4px 0px rgba(184, 184, 184, 0.03)',
+        'light-200': '10px 10px 20px 0px rgba(218, 213, 213, 0.10)',
+        'light-300': '-10px 10px 20px 0px rgba(218, 213, 213, 0.10)',
+        'dark-100': '0px 2px 10px 0px rgba(46, 52, 56, 0.10)',
+        'dark-200': '2px 0px 20px 0px rgba(39, 36, 36, 0.04)',
       },
     },
   },
