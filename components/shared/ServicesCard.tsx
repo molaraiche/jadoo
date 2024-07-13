d8c738ced527d2c51b753038725d6063b6006696
molaraiche <laraichemohamed@gmail.com>
2024-07-13 16:08:39 +0100

    updates: set the services side

--- components/shared/ServicesCard.tsx
+++ components/shared/ServicesCard.tsx
@@ -4,10 +4,18 @@ import React from 'react';
 
 const ServicesCard = ({ id, image, title, content }: servicesProps) => {
   return (
-    <div className='flex flex-col items-center justify-center box-shadow-light-300'>
-      <Image src={image} alt={content} width={80} height={80} />
-      <h1> {title} </h1>
-      <p className=''> {content} </p>
+    <div className='flex flex-col items-center justify-center box-shadow-light-300 cursor-pointer  h-[250px] shadow-main rounded-md w-[400px] sm:w-[250px] sm:h-[350px]'>
+      <Image
+        src={image}
+        alt={content}
+        width={100}
+        height={100}
+        className='object-cover'
+      />
+      <h3 className='text-base font-bold   text-[#1E1D4C]'> {title} </h3>
+      <p className='w-[50%] text-center leading-6 text-primary-textColor mt-2 '>
+        {content}
+      </p>
     </div>
   );
 };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -31,12 +31,7 @@ const config: Config = {
       },
       fontSize: {},
       boxShadow: {
-        'light-100':
-          '0px 12px 20px 0px rgba(184, 184, 184, 0.03), 0px 6px 12px 0px rgba(184, 184, 184, 0.02), 0px 2px 4px 0px rgba(184, 184, 184, 0.03)',
-        'light-200': '10px 10px 20px 0px rgba(218, 213, 213, 0.10)',
-        'light-300': '-10px 10px 20px 0px rgba(218, 213, 213, 0.10)',
-        'dark-100': '0px 2px 10px 0px rgba(46, 52, 56, 0.10)',
-        'dark-200': '2px 0px 20px 0px rgba(39, 36, 36, 0.04)',
+        main: ' rgba(0, 0, 0, 0.1) 0px 4px 12px;',
       },
       fontWeight: {
         links: '600',
