11a26074055ff153c90c92dd973b307eb53d496d
molaraiche <laraichemohamed@gmail.com>
2024-07-22 05:06:36 +0100

    updates

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,28 @@
-const path = require('path');
+import path from 'path';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
-      '@': path.resolve(__dirname),
-      '@/components': path.resolve(__dirname, 'components'),
-      '@/constants': path.resolve(__dirname, 'constants'),
-      '@/styles': path.resolve(__dirname, 'styles'),
-      '@/utils': path.resolve(__dirname, 'utils'),
+      '@': path.resolve('./'),
+      '@/components': path.resolve('./components'),
+      '@/constants': path.resolve('./constants'),
+      '@/styles': path.resolve('./styles'),
+      '@/utils': path.resolve('./utils'),
     };
     return config;
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+        port: '',
+        pathname: '*',
+      },
+    ],
+  },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
